bc32399a6b52a7233e1003859ff5a39967270d42
ljw2024polyu <24048007r@connect.polyu.hk>
2025-09-22 16:42:55 +0800

    build: copy landing and assets into site

--- build.bat
+++ build.bat
@@ -1,17 +1,31 @@
 @echo off
-REM clean
-if exist site rmdir /s /q site
-mkdir site
+setlocal
 
-REM build HPR-LP
-sphinx-build -b html docs\hprlp site\hprlp
+REM === Resolve paths relative to this script ===
+set "ROOT=%~dp0"
+set "SITE=%ROOT%site"
+set "DOCS=%ROOT%docs"
+set "LANDING=%ROOT%landing"
+set "ASSETS=%ROOT%assets"
 
-REM build HPR-QP
-sphinx-build -b html docs\hprqp site\hprqp
+REM === Clean site ===
+if exist "%SITE%" rmdir /s /q "%SITE%"
+mkdir "%SITE%"
 
-REM copy landing
-copy /y landing\index.html site\index.html
+REM === Build Sphinx projects ===
+sphinx-build -b html "%DOCS%\hprlp" "%SITE%\hprlp"
+sphinx-build -b html "%DOCS%\hprqp" "%SITE%\hprqp"
 
-echo Done. Open site\index.html
+REM === Copy landing files to site root (index.html, CSS, JS, images, etc.) ===
+REM robocopy is reliable and included in Windows
+robocopy "%LANDING%" "%SITE%" *.* /E /NFL /NDL /NJH /NJS /NP >nul
 
+REM === Mirror assets (avatars, images, css/js if any) to site/assets ===
+robocopy "%ASSETS%" "%SITE%\assets" /MIR /NFL /NDL /NJH /NJS /NP >nul
+
+REM === Ensure .nojekyll so _static/_sources are served on GitHub Pages ===
+type nul > "%SITE%\.nojekyll"
+
+echo Done. Open "%SITE%\index.html"
 pause
+
